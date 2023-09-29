66315960766575c50e38ba040ac445ebbe2c732d
Reza Tadi <90446850+tadireza@users.noreply.github.com>
2023-09-29 12:24:14 +0330

    Practice working with numbers
    تمرین کار با اعداد در جاوااسکریپت

---
+++ 01-Practice working with numbers/main.js
@@ -0,0 +1,10 @@
+/*I want to calculate the circumference of a circle. */
+const radius = 5;
+const circumference = 2* Math.PI* radius;
+console.log(circumference);
+
+
+/*I want to calculate the area of a circle. */
+const r=6;
+const area= Math.PI*Math.pow(r,2);
+console.log(area);
