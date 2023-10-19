7eb0c3a4b8a9ee6dd401aacb335d7ce3294dbebd
Reza Tadi <90446850+tadireza@users.noreply.github.com>
2023-10-19 23:36:39 +0330

    destructring arrays

---
+++ 16-destructering exercise/main.js
@@ -0,0 +1,10 @@
+const fullStack = [
+    ["html", "css", "JS", "React"],
+    ["Node.js", "express", "MongoDB"]
+];
+
+const [front, back] = fullStack;
+
+const [ , , js, react] = front;
+console.log(js , react);
+
